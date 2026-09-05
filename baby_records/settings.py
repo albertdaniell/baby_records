a4c8261052e023a9742147ea91e22a309dca905b
albertdaniell <albertagoya@gmail.com>
2026-09-05 10:43:56 +0300

    fixing

--- baby_records/settings.py
+++ baby_records/settings.py
@@ -86,6 +86,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baby_glucose',
+        'USER': 'admin',
+        'PASSWORD': 'D@t@sc13nc3',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
